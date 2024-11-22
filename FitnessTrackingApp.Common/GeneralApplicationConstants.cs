dcb2b45ec24fd1278e9b18bda4fa672fb2000e9e
Anzhel-Petrov <anzhel_petrov@hotmail.com>
2024-11-22 20:36:41 +0200

    Add WebApplicationBuilderExtensions for admin/trainer roles seed

---
+++ FitnessTrackingApp.Common/GeneralApplicationConstants.cs
@@ -0,0 +1,14 @@
+﻿namespace FitnessTrackingApp.Common;
+
+public static class GeneralApplicationConstants
+{
+    public const string AdminAreaName = "Admin";
+    public const string AdminRoleName = "Administrator";
+    public const string DevelopmentAdminEmail = "admin@admin.com";
+    
+    public const string TrainerAreaName = "Admin";
+    public const string TrainerRoleName = "Administrator";
+    public const string DevelopmentTrainerEmail = "trainer@admin.com";
+    
+    public const string UserRoleName = "User";
+}
---
+++ FitnessTrackingApp.Web.Infrastructure/Extensions/WebApplicationBuilderExtensions.cs
@@ -0,0 +1,66 @@
+﻿using FitnessTrackingApp.Data.Models;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
+using static FitnessTrackingApp.Common.GeneralApplicationConstants;
+
+namespace FitnessTrackingApp.Web.Infrastructure.Extensions;
+
+public static class WebApplicationBuilderExtensions
+{
+    public static IApplicationBuilder SeedUserRole(this IApplicationBuilder app, string userId)
+    {
+        using IServiceScope scopedServices = app.ApplicationServices.CreateScope();
+
+        //Get DI container service provider
+        IServiceProvider serviceProvider = scopedServices.ServiceProvider;
+        UserManager<ApplicationUser> userManagaer = serviceProvider.GetRequiredService<UserManager<ApplicationUser>>();
+        RoleManager<IdentityRole<Guid>> roleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole<Guid>>>();
+
+        Task.Run(async () =>
+            {
+                if (await roleManager.RoleExistsAsync(AdminRoleName))
+                {
+                    return;
+                }
+                IdentityRole<Guid> role = new IdentityRole<Guid>(AdminRoleName);
+                await roleManager.CreateAsync(role);
+
+                ApplicationUser userToFind = await userManagaer.FindByIdAsync(userId);
+                await userManagaer.AddToRoleAsync(userToFind, AdminRoleName);
+
+            })
+            .GetAwaiter()
+            .GetResult();
+        // To do chaining
+        return app;
+    }
+    
+    public static IApplicationBuilder SeedTrainer(this IApplicationBuilder app, string userId)
+    {
+        using IServiceScope scopedServices = app.ApplicationServices.CreateScope();
+
+        //Get DI container service provider
+        IServiceProvider serviceProvider = scopedServices.ServiceProvider;
+        UserManager<ApplicationUser> userManagaer = serviceProvider.GetRequiredService<UserManager<ApplicationUser>>();
+        RoleManager<IdentityRole<Guid>> roleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole<Guid>>>();
+
+        Task.Run(async () =>
+            {
+                if (await roleManager.RoleExistsAsync(AdminRoleName))
+                {
+                    return;
+                }
+                IdentityRole<Guid> role = new IdentityRole<Guid>(AdminRoleName);
+                await roleManager.CreateAsync(role);
+
+                ApplicationUser userToFind = await userManagaer.FindByIdAsync(userId);
+                await userManagaer.AddToRoleAsync(userToFind, AdminRoleName);
+
+            })
+            .GetAwaiter()
+            .GetResult();
+        // To do chaining
+        return app;
+    }
+}
